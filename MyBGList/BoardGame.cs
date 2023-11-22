d917f95500ce27f894be7cfe08a494363c9d1179
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-11-22 17:57:56 +0100

    Initial commit

---
+++ MyBGList/BoardGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyBGList
+{
+	public class BoardGame
+	{
+		public int Id { get; set; }
+
+		public string? Name { get; set; }
+
+		public int? Year { get; set; }
+	}
+}
+
